1ad98282dd6eefc027882d6312d255bf36cd3fdc
giuliamelis24 <melis.giulia@iisgalvanimi.edu.it>
2026-04-13 09:05:21 +0000

    funzione orario

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from datetime import datetime
+
 
 app = FastAPI()
 
-# Spieghiamo a FastAPI che i file dentro "static" sono accessibili
+
+# Serve per rendere accessibili CSS e JS
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/") #app è un end point che restituisce la home page 
-def home():
-    # Restituisce direttamente il file HTML
-    return FileResponse('static/index.html')
-    from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-# Spieghiamo a FastAPI che i file dentro "static" sono accessibili
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def home():
-    # Restituisce direttamente il file HTML
-    return FileResponse('static/index.html')
+    return FileResponse('static/home.html')
 
-from datetime import datetime
 
 @app.get("/ora")
 def dammi_ora():
-    # Restituiamo un dizionario (JSON)
     return {"orario": datetime.now().strftime("%H:%M:%S")}
+
+
+# NUOVO: Endpoint con parametro di query
+@app.get("/saluta")
+def saluta_utente(nome: str):
+    return {"messaggio": f"Ciao {nome}, benvenuto nel server di Terza!"}
